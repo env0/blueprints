26281841c4b10ef5c47ed0eb3316bec49aad3f19
Ohad Maislish <ohad1282@gmail.com>
2019-05-29 14:16:28 +0300

    changed to medium

--- redash/v5.0.2/aws/ec2.tf
+++ redash/v5.0.2/aws/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "redash-instance" {
   ami = "ami-0c654c3ab463d22f6"
-  instance_type = "t2.small"
+  instance_type = "t2.medium"
   subnet_id = "${aws_subnet.redash-subnet.id}"
   vpc_security_group_ids = ["${aws_security_group.redash-sg.id}"]
   key_name = "${aws_key_pair.redash-key-pair.key_name}"
