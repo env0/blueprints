98d736650c649fe39849ddc407ee6afba162fbcc
Marcelo Luiz Onhate <marcelo.onhate@env0.com>
2019-07-01 21:58:24 -0300

    fix e2e template

--- e2e/v0.0.1/variables/output-variables.tf
+++ e2e/v0.0.1/variables/output-variables.tf
@@ -1,27 +1,18 @@
-provider "aws" {
-  region = var.aws_default_region
-}
-
-# Using these data sources allows the configuration to be
-# generic for any region.
+# using these data sources allows the configuration to be generic for any region.
 data "aws_region" "current" {}
-
 data "aws_availability_zones" "available" {}
 
-variable "terraform_variable" {
-  default = "terraform_variable_NOT_SET"
-}
-variable "environment_variable" {
-  default = "environment_variable_NOT_SET"
-}
-variable "aws_default_region" {
-  default = "us-east-1"
+variable "terraform_variable" { default = "terraform_variable_NOT_SET" }
+variable "environment_variable" { default = "environment_variable_NOT_SET" }
+
+provider "aws" {
+  region = "us-east-1"
 }
 
 output "terraform_variable" {
-  value = var.terraform_variable
+  value = "${var.terraform_variable}"
 }
 
 output "environment_variable" {
-  value = var.environment_variable
+  value = "${var.environment_variable}"
 }
