72e37ed9b8b5f8feea96dc7d671d9f4fb96e28f3
Roni Frantchi <roni.frantchi@gmail.com>
2019-08-26 12:39:56 +0300

    use jinja2 cli to generate stage tfvars

--- redash/v5.0.2/aws/ec2.tf
+++ redash/v5.0.2/aws/ec2.tf
@@ -1,13 +1,14 @@
 resource "aws_instance" "redash-instance" {
   ami = "ami-0c654c3ab463d22f6"
-  instance_type = "t2.small"
-  subnet_id = "${aws_subnet.redash-subnet.id}"
-  vpc_security_group_ids = ["${aws_security_group.redash-sg.id}"]
-  key_name = "${aws_key_pair.redash-key-pair.key_name}"
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.redash-subnet.id
+  vpc_security_group_ids = [aws_security_group.redash-sg.id]
+  key_name = aws_key_pair.redash-key-pair.key_name
 
   tags {
     Name = "redash-instance"
     CreatedBy = "env0"
-    Env0 = "${random_uuid.uuid.result}"
+    Stage = var.stage
+    Env0 = random_uuid.uuid.result
   }
 }
