27499793b9884956f0d6b7a9349b0403c691ebc0
avnersorek <avner.sorek@env0.com>
2019-09-10 12:03:50 +0300

    new approach

--- azure-ssh-key/main.tf
+++ azure-ssh-key/main.tf
@@ -1,3 +1,5 @@
+# adapted from https://docs.microsoft.com/en-us/azure/virtual-machines/linux/terraform-create-complete-vm
+
 provider "azurerm" {}
 
 variable "ssh_public_key" {}
@@ -8,74 +10,101 @@ resource "random_string" "random" {
   upper   = false
 }
 
-resource "azurerm_resource_group" "resource_group" {
-  name     = "env0-ssh-key-${random_string.random.result}"
-  location = "northeurope"
+
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = "env0-ssh-key-${random_string.random.result}"
+    location = "eastus"
 }
 
-resource "azurerm_virtual_network" "virtual_network" {
-  name                = "${azurerm_resource_group.resource_group.name}-vn"
-  address_space       = ["10.0.0.0/16"]
-  location            = "${azurerm_resource_group.resource_group.location}"
-  resource_group_name = "${azurerm_resource_group.resource_group.name}"
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "${azurerm_resource_group.myterraformgroup.name}-vn"
+    address_space       = ["10.0.0.0/16"]
+    location            = "eastus"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "${azurerm_resource_group.resource_group.name}-subnet"
-  resource_group_name  = "${azurerm_resource_group.resource_group.name}"
-  virtual_network_name = "${azurerm_virtual_network.virtual_network.name}"
-  address_prefix       = "10.0.2.0/24"
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "${azurerm_resource_group.myterraformgroup.name}-subnet"
+    resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
+    virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
+    address_prefix       = "10.0.1.0/24"
 }
 
-resource "azurerm_virtual_machine_scale_set" "test" {
-  name                = "${azurerm_resource_group.resource_group.name}-scale-set"
-  location            = "${azurerm_resource_group.resource_group.location}"
-  resource_group_name = "${azurerm_resource_group.resource_group.name}"
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "${azurerm_resource_group.myterraformgroup.name}-public-ip"
+    location                     = "eastus"
+    resource_group_name          = "${azurerm_resource_group.myterraformgroup.name}"
+    allocation_method            = "Dynamic"
+}
 
-  network_profile {
-    name    = "terraformnetworkprofile"
-    primary = true
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "${azurerm_resource_group.myterraformgroup.name}-sg"
+    location            = "eastus"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+    
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+}
+
+resource "azurerm_network_interface" "myterraformnic" {
+    name                      = "${azurerm_resource_group.myterraformgroup.name}-nic"
+    location                  = "eastus"
+    resource_group_name       = "${azurerm_resource_group.myterraformgroup.name}"
+    network_security_group_id = "${azurerm_network_security_group.myterraformnsg.id}"
 
     ip_configuration {
-      name      = "TestIPConfiguration"
-      primary   = true
-      subnet_id = "${azurerm_subnet.subnet.id}"
+        name                          = "myNicConfiguration"
+        subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"
     }
-  }
+}
 
-  os_profile {
-    computer_name_prefix = "testvm"
-    admin_username       = "myadmin"
-  }
+resource "azurerm_virtual_machine" "myterraformvm" {
+    name                  = "${azurerm_resource_group.myterraformgroup.name}-vm"
+    location              = "eastus"
+    resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
+    network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
+    vm_size               = "Standard_DS1_v2"
 
-  os_profile_linux_config {
-    disable_password_authentication = true
-
-    ssh_keys {
-      path     = "/home/myadmin/.ssh/authorized_keys"
-      key_data = "${var.ssh_public_key}"
+    storage_os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Premium_LRS"
     }
-  }
 
-  sku {
-    name     = "Standard_F2"
-    tier     = "Standard"
-    capacity = 1
-  }
+    storage_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "16.04.0-LTS"
+        version   = "latest"
+    }
 
-  storage_profile_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
+    os_profile {
+        computer_name  = "myvm"
+        admin_username = "azureuser"
+    }
 
-  storage_profile_os_disk {
-    name              = ""
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
+    os_profile_linux_config {
+        disable_password_authentication = true
+        ssh_keys {
+            path     = "/home/azureuser/.ssh/authorized_keys"
+            key_data = "${var.ssh_public_key}"
+        }
+    }
+}
 
-  upgrade_policy_mode = "Automatic"
+
+output "public_ip" {
+  value = "${azurerm_public_ip.myterraformpublicip.ip_address}"
 }
