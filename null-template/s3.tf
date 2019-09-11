6760f08e5dcd50e8ea25a2e3b3473fabbe8e007f
omryhay1 <omry@env0.com>
2019-09-11 15:24:36 +0300

    added s3 bucket and s3 remote backend

--- null-template/s3.tf
+++ null-template/s3.tf
@@ -1,10 +1,13 @@
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
-  acl    = "private"
+  bucket = "env0-my-tf-test-bucket"
+  acl    = "public-read"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["https://s3-website-test.hashicorp.com"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
   }
 }
 
