9073009566667c07c4bbcdf2c93542214f4fde3e
omryhay1 <omry@env0.com>
2019-11-08 13:40:53 +0200

    added assume role

--- s3-remote-backend/main.tf
+++ s3-remote-backend/main.tf
@@ -1,3 +1,11 @@
+provider "aws" {
+  assume_role {
+    role_arn     = "arn:aws:iam::326535729404:role/env0_test_app"
+    session_name = "env0_session"
+    external_id  = "env0"
+  }
+}
+
 terraform {
   backend "s3" {
     bucket = "env0-userland-remote-state-bucket"
