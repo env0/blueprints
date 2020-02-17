11015f14101e78cefc15d230c2f348aae33e53ff
omryhay1 <omry@env0.com>
2020-02-17 23:44:26 +0200

    added redash output

--- private.tf
+++ private.tf
@@ -13,3 +13,7 @@ module "redash" {
   source = "git::git@github.com:env0/blueprints.git//redash/v5.0.2/aws?ref=redash-module"
   aws_default_region = var.aws_region
 }
+
+output "redash_ip" {
+  value = module.redash.instance_public_ip
+}
