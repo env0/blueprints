04aa150d6126942d2e4cba58f39ecd2f4d1ff0c8
omryhay1 <omry@env0.com>
2020-02-17 23:33:50 +0200

    removed tags

--- redash/v5.0.2/aws/ec2.tf
+++ redash/v5.0.2/aws/ec2.tf
@@ -4,10 +4,4 @@ resource "aws_instance" "redash-instance" {
   subnet_id = aws_subnet.redash-subnet.*.id[0]
   vpc_security_group_ids = [aws_security_group.redash-sg.id]
   key_name = aws_key_pair.redash-key-pair.key_name
-
-  tags = {
-    Name = "redash-instance"
-    CreatedBy = "env0"
-    Env0 = random_uuid.uuid.result
-  }
 }
