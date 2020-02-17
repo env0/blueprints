a8dfa85c7733231541f67f4e0f999ed439054dd3
omryhay1 <omry@env0.com>
2020-02-17 23:30:36 +0200

    fixed tags

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 # Output variable: Public IP address
 output "public_ip" {
-  value = "${google_compute_instance.example.network_interface.0.access_config.0.nat_ip}"
+  value = google_compute_instance.example.network_interface[0].access_config[0].nat_ip
 }
