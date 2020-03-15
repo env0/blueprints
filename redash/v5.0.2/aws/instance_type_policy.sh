45abd4dcaea37af6893b7c67363d35434ae45b08
omryhay1 <omry@env0.com>
2020-03-15 12:50:38 +0200

    some fixes

---
+++ redash/v5.0.2/aws/instance_type_policy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+function in_array {
+  ARRAY=$2
+  for e in ${ARRAY[*]}
+  do
+    if [[ "$e" == "$1" ]]
+    then
+      return 0
+    fi
+  done
+  return 1
+}
+
+allowedInstances=("t2.nano" "t2.micro" "t2.small" "t2.medium")
+
+if in_array $1 "${allowedInstances[*]}"
+  then
+    echo "$1 instance is allowed"
+  else
+    exit 1
+fi
