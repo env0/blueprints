cb34495a2a948a83e6ecd4e34c5fad615d11980e
Omry Hay <46656490+omryhay1@users.noreply.github.com>
2020-03-16 20:44:41 +0200

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -9,11 +9,6 @@ variable "company_name" {
   default = "Hooli"
 }
 
-variable "company_logo" {
-  description = "The logo of the company to greet"
-  default = "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-original-577x577/s3/032018/untitled-1_402.png?jE55z5k8TLCYRuash4OvNAA5c7zSvs_Y&itok=jshT6m-f"
-}
-
 variable "aws_region" {
   description = "The AWS region you would like Redash to be installed in"
   default = "us-east-1"
