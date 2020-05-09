f4ee49fe24cecf7f2a6819d2146872106f08a733
omryhay1 <omry@env0.com>
2020-05-09 22:07:00 +0300

    added template with many resources

---
+++ many-resources/main.tf
@@ -0,0 +1,19 @@
+resource "null_resource" "null" {
+  count = 50
+}
+
+resource "null_resource" "null2" {
+  count = 20
+}
+
+resource "random_uuid" "test" {
+  count = 100
+}
+
+resource "random_pet" "pet" {
+  count = 60
+}
+
+resource "time_static" "example" {
+  count = 10
+}
