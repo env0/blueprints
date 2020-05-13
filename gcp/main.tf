67a4b3636e70e34f1b0f1436bac634078ac82ce3
Yaron Yarimi <yaronya@users.noreply.github.com>
2020-05-14 00:44:15 +0300

    Update main.tf

--- gcp/main.tf
+++ gcp/main.tf
@@ -4,9 +4,35 @@ provider "google" {
   zone    = "us-east1-b"
 }
 
-resource "google_compute_instance" "google-i" {
-  name         = "gcp-test"
-  machine_type = "f1-micro"
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t3.small"
+}
+
+resource "google_compute_instance" "yaron-test" {
+  name         = "yaron-test"
+  machine_type = "n1-standard-1"
 
   boot_disk {
     initialize_params {
