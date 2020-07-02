32f97bf2493aae2ade496254045d5acfdfd83a3a
Avner Sorek <53464600+avnerenv0@users.noreply.github.com>
2020-07-02 21:51:49 +0300

    Update output.tf

--- attempt/output.tf
+++ attempt/output.tf
@@ -1,5 +1,14 @@
-output "url" {
+output "obj_out" {
   value = {
     dog = 2
   }
+}
+
+output "array_out" {
+  value = [ "this", "is", "an", "Array" ]
+}
+
+
+output "string_out" {
+  value = "Just a regular string"
 }
