30e8be0920f7ea77f7aaf19ae0e72390b6152ed9
omryhay1 <omry@env0.com>
2020-07-16 15:19:49 +0300

    trying with terraform

--- null-template/test.sh
+++ null-template/test.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+
 JSON_OUTPUT=$(curl 'https://api.github.com/')
 echo ${JSON_OUTPUT}
-KEY=$(curl 'https://api.github.com/' | npx jq.node 'get("authorizations_url") | toString')
+export KEY=$(echo ${JSON_OUTPUT} | jq -r .somekey)
 echo ${KEY}
+#KEY=$(curl 'https://api.github.com/' | npx jq.node 'get("authorizations_url") | toString')
+#echo ${KEY}
