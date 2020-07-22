7f04f2dcfbb2094332fd5ebb2286158e8f3390ed
omryhay1 <omry@env0.com>
2020-07-22 11:59:53 +0300

    added failed destory resource

--- null-template/main.tf
+++ null-template/main.tf
@@ -1,17 +1,9 @@
-resource "null_resource" "null" {
-}
-
-resource "null_resource" "null2" {
-}
-
 resource "null_resource" "example1" {
-  provisioner "local-exec" {
-    command = "./test.sh"
-  }
 }
 
 resource "null_resource" "example2" {
   provisioner "local-exec" {
-    command = "./test.sh"
+    when    = "destroy"
+    command = "exit 1"
   }
 }
--- null-template/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-
-JSON_OUTPUT=$(curl 'https://api.github.com/')
-echo ${JSON_OUTPUT}
-export KEY=$(echo ${JSON_OUTPUT} | npx jq.node 'get("authorizations_url") | toString')
-echo ${KEY}
